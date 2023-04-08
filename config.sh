04c6305b71a0615effcaa7ab402b16cac816529d
Danilo Nascimento <daconnas.dcn@gmail.com>
2023-04-08 09:19:48 -0300

    adicionando configuração do i3status

--- config.sh
+++ config.sh
@@ -1,4 +1,5 @@
 ln -sf "$(pwd)/lvim" ~/.config/lvim
 ln -sf "$(pwd)/nvim" ~/.config/nvim
 ln -sf "$(pwd)/i3" ~/.config/i3
+ln -sf "$(pwd)/i3status" ~/.config/i3status
 ln -sf "$(pwd)/i3blocks" ~/.config/i3blocks
