b6e45cb43314dffbd0d04c63078351852dc39c0d
Danilo da Conceição Nascimento <daconnas.dcn@gmail.com>
2023-07-14 11:24:48 -0300

    adicionando binários ao $PATH

--- config.sh
+++ config.sh
@@ -3,3 +3,6 @@ ln -sf "$(pwd)/nvim" ~/.config/nvim
 ln -sf "$(pwd)/i3" ~/.config/i3
 ln -sf "$(pwd)/i3status" ~/.config/i3status
 ln -sf "$(pwd)/i3blocks" ~/.config/i3blocks
+
+chmod +x bin/*
+cp bin/* ~/.local/bin
